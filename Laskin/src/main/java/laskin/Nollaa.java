a8ddcbc7240fe9d517f7d160fd03ee52ef33b2a3
Jami Lehtinen <jami.lehtinen1@gmail.com>
2018-12-09 23:22:56 +0200

    viikko 6 tehtävä 1

---
+++ Laskin/src/main/java/laskin/Nollaa.java
@@ -0,0 +1,42 @@
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Nollaa implements Komento {
+	private TextField tuloskentta; 
+    private TextField syotekentta; 
+	private Button nollaa;
+    private Button undo;
+    private Sovelluslogiikka sovellus;
+
+	public Nollaa(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+		this.tuloskentta = tuloskentta;
+        this.syotekentta = syotekentta;
+		this.nollaa = nollaa;
+        this.undo = undo;
+        this.sovellus = sovellus;
+	}
+
+	@Override
+	public void suorita() {
+		sovellus.nollaa();
+		
+		int laskunTulos = sovellus.tulos();
+        
+        syotekentta.setText("");
+        tuloskentta.setText("" + laskunTulos);
+		
+		if (laskunTulos == 0) {
+            nollaa.disableProperty().set(true);
+        } else {
+            nollaa.disableProperty().set(false);
+        }
+	}
+
+	@Override
+	public void peru() {
+		System.out.println("undo pressed");
+	}
+	
+}
---
+++ Laskin/src/main/java/laskin/Tapahtumankuuntelija.java
@@ -0,0 +1,43 @@
+package laskin;
+
+import java.util.HashMap;
+import java.util.Map;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Tapahtumankuuntelija implements EventHandler {
+    private TextField tuloskentta; 
+    private TextField syotekentta; 
+	private Sovelluslogiikka sovellus;
+	private Button undo;
+    
+	private Map<Button, Komento> komennot;
+    private Komento edellinen = null;
+
+    public Tapahtumankuuntelija(TextField tuloskentta, TextField syotekentta, Button plus, Button miinus, Button nollaa, Button undo) {
+        this.sovellus = new Sovelluslogiikka();
+		this.komennot = new HashMap<>();
+		this.undo = undo;
+		
+		komennot.put(plus, new Summa(tuloskentta, syotekentta,  nollaa, undo, sovellus));
+		komennot.put(miinus, new Erotus(tuloskentta, syotekentta, nollaa, undo, sovellus) );
+        komennot.put(nollaa, new Nollaa(tuloskentta, syotekentta,  nollaa, undo, sovellus) );
+    }
+    
+    @Override
+    public void handle(Event event) {
+		if (event.getTarget() != undo) {
+			Komento komento = komennot.get((Button) event.getTarget());
+			komento.suorita();
+			edellinen = komento;
+		} else {
+			edellinen.peru();
+			edellinen = null;
+		}
+		
+        undo.disableProperty().set(false);
+    }
+
+}
